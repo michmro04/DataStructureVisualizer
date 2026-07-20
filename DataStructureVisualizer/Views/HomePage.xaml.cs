4f2f751b31cd894880376c03309329e34e2fc8d9
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-07-20 22:59:53 +0200

    adding new directionaries and HomePage

---
+++ DataStructureVisualizer/Views/HomePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataStructureVisualizer.Views
+{
+
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void StackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new StackPage());
+        }
+
+        private void QueueBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new QueuePage());
+        }
+
+    }
+}
