cb7ea7ce2a1309ac8b5d824a0d35b4fb75ccedc4
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-07-20 23:09:34 +0200

    adding Interface of all structures

---
+++ DataStructureVisualizer/Services/IVisualizableStructure.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructureVisualizer.Services
+{
+    public interface IVisualizableStructure
+    {
+        // Generic name to unify Push (Stack), Enqueue (Queue), and Insert (List)
+        void AddElement(int element);
+
+        // Generic name to unify Pop (Stack), Dequeue (Queue), and Delete (List)
+        int RemoveElement(int element);
+
+        // Provides the current sequence of elements for the UI to draw
+        IEnumerable<int> GetElements();
+
+        // Clears all elements from the structure
+        void Clear();
+    }
+}
