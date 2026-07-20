884cff637ac147ad4b91141b485ef478f5c025a3
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-07-20 23:30:45 +0200

    Adding MyStack class with internal private Node class - begining of logic of stack structure

---
+++ DataStructureVisualizer/Structures/MyStack.cs
@@ -0,0 +1,49 @@
+﻿using DataStructureVisualizer.Services;
+
+
+namespace DataStructureVisualizer.Structures
+{
+    public class MyStack : IVisualizableStructure
+    {
+        private class Node
+        {
+            public int Value { get; set;  }
+            public Node Next { get; set; }
+
+            public Node(int value)
+            {
+                Value = value;
+                Next = null;
+            }
+        }
+
+
+        private Node _top;
+        public MyStack()
+        {
+            _top = null;
+        }
+
+
+        public void AddElement(int element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveElement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> GetElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
